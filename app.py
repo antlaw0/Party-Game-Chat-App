ebc83068b1db21f9816925dcc8de19abaff41ee4
AAA <aaa@aaa.com>
2017-12-13 13:33:08 -0600

    deploy

--- app.py
+++ app.py
@@ -13,23 +13,11 @@ def home_page():
 
 @socketio.on('new_message')
 def handle_new_message(message):
-    print("New message recieved: "+ str(request.sid)+ " " + message)
+    print("New message received: "+message)
     # Broadcast the messsage to all connected clients.
-    emit("new_message_received", str(request.sid)+" "+message, broadcast=True, room=request.sid)
+    emit("new_message_received", str(request.sid)+" "+message, broadcast=True)
 
-@socketio.on('join')
-def on_join(data):
-    username = data['username']
-    room = data['room']
-    join_room(room)
-    send(username + ' has entered the room.', room=room)
 
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    send(username + ' has left the room.', room=room)
 	
 if __name__ == '__main__':
     socketio.run(app)
